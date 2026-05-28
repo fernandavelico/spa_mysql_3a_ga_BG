e55ba51f2ec0bace48769add976df81f3714c429
fernandavelico <velicofernanda@gmail.com>
2026-05-28 20:28:00 +0000

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,50 @@ function App(){
   const [editando, setEditando] = useState(false)
   const [idAtual, setIdAtual] = useState(null)
 
+
+  async function buscarAlunos() {
+    const resposta = await axios.get('https://opulent-space-train-7vgjqv6gwvwvcgw6-3001.app.github.dev/alunos')
+    setAlunos(resposta.data)
+  }
+
+  useEffect(()=>{buscarAlunos();},[])
+
+  async function salvar(e) {
+    e.preventDefault()
+    const aluno = {nome, email, curso}
+
+    if(editando){
+      await axios.put(`https://opulent-space-train-7vgjqv6gwvwvcgw6-3001.app.github.dev/alunos/${idAtual}`, aluno)
+      setEditando(false)
+      setIdAtual(null)
+    }
+    else{
+      await axios.post('https://opulent-space-train-7vgjqv6gwvwvcgw6-3001.app.github.dev/alunos', aluno)
+    }
+
+    limparFormulario()
+    buscarAlunos()
+  }
+
+    async function excluir(id) {
+      await axios.delete(`https://opulent-space-train-7vgjqv6gwvwvcgw6-3001.app.github.dev/alunos/${id}`, alunos)
+      buscarAlunos()
+    }
+
+    function limparFormulario(){
+      setNome('')
+      setEmail('')
+      setCurso('')
+    }
+
+    function editar(aluno){
+      setNome(aluno.nome)
+      setEmail(aluno.email)
+      setCurso(aluno.curso)
+      setIdAtual(aluno.id)
+      setEditando(true)
+    }
+
   return(
     <div style={{padding:20}}>
       <h1>CRUD de Alunos</h1>
@@ -42,6 +86,20 @@ function App(){
           {editando ? "Atualizar" : "Cadastrar"}
         </button>
       </form>
+
+      <hr/> 
+      {
+        alunos.map((aluno)=>(
+          <div key={aluno.id}>
+          <h3>{aluno.nome}</h3>
+          <p>{aluno.email}</p>
+          <p>{aluno.curso}</p>
+          <button onClick={()=> editar(aluno)}>Editar</button>
+          <button onClick={()=> excluir(aluno.id)}>Excluir</button>
+          <hr/>
+          </div>
+        ))
+      }
     </div>
   )
 }
